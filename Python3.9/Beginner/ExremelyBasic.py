1303ab197001b5f8cf2023e9d8862346ed010f8f
Artur Nascimento <arturncarmo@gmail.com>
2021-05-04 10:22:00 -0300

    Create ExremelyBasic.py

---
+++ Python3.9/Beginner/ExremelyBasic.py
@@ -0,0 +1,12 @@
+'''Read 2 variables, named A and B and make the sum of these two variables,
+assigning its result to the variable X. Print X as shown below. Print endline 
+after the result otherwise you will get “Presentation Error”.'''
+
+#======# MAIN #======#
+
+#Read A and B
+a = int(input())
+b = int(input())
+
+#Print the result
+print("X =", a+b)
